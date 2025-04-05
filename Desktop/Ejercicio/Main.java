a76241a1394a2e29a5b08d6cd66e308a54fe13f2
Elmer001 <00130518@uca.edu.sv>
2025-04-05 14:15:00 -0600

    generacion de jugadores agregada

--- Desktop/Ejercicio/Main.java
+++ Desktop/Ejercicio/Main.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.Random;
 
 public class Main {
 
@@ -19,6 +18,9 @@ public class Main {
             this.apellido = apellido;
         }
 
+        public Jugador() {
+        }
+
         // Getters
         public String getNombre() {
             return nombre;
@@ -32,11 +34,11 @@ public class Main {
             return id;
         }
 
-        public String getGolesMarcados() {
+        public int getGolesMarcados() {
             return golesMarcados;
         }
         
-        public String getPartidosJugados() {
+        public int getPartidosJugados() {
             return partidosJugados;
         }
 
@@ -51,30 +53,18 @@ public class Main {
                     '}';
         }
 
-        //metodo para generera jugadores
-        public List<Jugador> generarJugadores(){
-            List<String> nombres = new ArrayList<>();
-            nombres.add("Luis");
-            nombres.add("Juan");
-            nombres.add("Maria");
-            nombres.add("Carlos");
-                
-            List<String> aplellidos = new ArrayList<>();
-            nombres.add("Sanchez");
-            nombres.add("Perez");
-            nombres.add("Torrez");
-            nombres.add("Lopez");
+// Método para generar una lista de jugadores
+public List<Jugador> generarJugadores() {
+    List<String> nombres = Arrays.asList("Luis", "Juan", "Maria", "Carlos");
+    List<String> apellidos = Arrays.asList("Sanchez", "Perez", "Torrez", "Lopez");
 
-            List<Jugador> jugadores = new ArrayList<>();
-            for (int i = 0; i < 5; i++){
-                String nombre = nombres.get((Math.random()*4) + 1);
-                String apellidos = aplellidos.get((Math.random()*4) + 1);
+    List<Jugador> jugadores = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+        jugadores.add(generarJugadorAleatorio(nombres, apellidos));
+    }
 
-                jugadores.add(new Jugador(nombre, apellidos, (Math.random()*4) + 1));
-            }
-
-            return jugadores;
-        }
+    return jugadores;
+}
     }
 
     // 1. Definición de la clase Partido
@@ -85,7 +75,7 @@ public class Main {
         private int golesEquipoB;
 
         // Constructor
-        public NewPartido(List<Jugador> equipoA, List<Jugador> equipoB, int golesEquipoA, int golesEquipoB) {
+        public Partido(List<Jugador> equipoA, List<Jugador> equipoB, int golesEquipoA, int golesEquipoB) {
             this.equipoA = equipoA;
             this.equipoA = equipoA;
             this.golesEquipoA = golesEquipoA;
@@ -94,20 +84,63 @@ public class Main {
 
         // Registrar goles
         public void registrarGoles() {
-            return nombre;
+            
         }
 
         public String equipoGanador() {
-            return id;
+            return "";
         }
 
         public String DetallesPartido() {
-            return golesMarcados;
+            return "";
         }
         
     }
+    //Método para generar un jugador aleatorio
+    public static Jugador generarJugadorAleatorio(List<String> nombres, List<String> apellidos) {
+    String nombre = nombres.get((int) (Math.random() * nombres.size()));
+    String apellido = apellidos.get((int) (Math.random() * apellidos.size()));
+    int id = (int) (Math.random() * 1000);
+    return new Jugador(nombre, apellido, id);
+}
 
     public static void main(String[] args) {
+        System.out.println("Opciones:");
+        System.out.println("1. Generar Partido");
+        System.out.println("2. Mostrar detalles del partido anterior");
+
+        switch (new Scanner(System.in).nextInt()) {
+            case 1:
+                // Generar jugadores
+                Jugador jugador = new Jugador();
+                List<Jugador> jugadores = jugador.generarJugadores();
+                
+                // Generar equipos
+                List<Jugador> equipoA = jugador.generarJugadores();
+                List<Jugador> equipoB = jugador.generarJugadores();
+
+                // Mostrar equipos
+                System.out.println("Equipo A:");
+                for (Jugador j : equipoA) {
+                    System.out.println(j);
+                }
+                System.out.println("Equipo B:");
+                for (Jugador j : equipoB) {
+                    System.out.println(j);
+                }
+
+                // Crear partido
+                Partido partido = new Partido(equipoA, equipoB, 0, 0);
+                
+                // Registrar goles
+                partido.registrarGoles();
+                
+                break;
+            case 2:
+                break;
         
+            default:
+                break;
+        }
     }
 }
